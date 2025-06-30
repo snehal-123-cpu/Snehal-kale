36c0edb3584a1b6d30ca416d3bd00cd2a008f35b
snehal-123-cpu <snehalmkale2005@gmail.com>
2025-06-30 16:19:33 +0530

    Add files via upload

---
+++ substring2.java
@@ -0,0 +1,10 @@
+public class substring2 {
+    public static void main(String[] args) {
+        String text = "snehal";
+        String sub = text.substring(0, 5);
+        System.out.println("Substring is: " + sub);
+
+        
+
+    }
+}
