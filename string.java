87df7c8358342c9e4ffaf7b2408842019e2b170a
snehal-123-cpu <snehalmkale2005@gmail.com>
2025-06-30 16:28:35 +0530

    Add files via upload

---
+++ string.java
@@ -0,0 +1,14 @@
+public class string {
+    public static void main(String[] args) {
+        String Name="Snehal";
+        String lastName="kale";
+        String fullName = Name+lastName;
+
+        for (int i = 0; i < fullName.length(); i++) {
+            System.out.print(fullName.charAt(i));
+        }
+    }
+}
+
+
+
