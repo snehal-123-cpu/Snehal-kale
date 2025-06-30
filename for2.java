8554d4c53eb3e2e5a5b0dd9826dcc4f34a63e1c0
snehal-123-cpu <snehalmkale2005@gmail.com>
2025-06-30 17:07:20 +0530

    Add files via upload

---
+++ for2.java
@@ -0,0 +1,12 @@
+public class for2{
+    public static void main(String[]arg){
+    for( int i=1; i<=10; i++){
+    if(i%2==0){
+    System.out.println(i+" even");
+    }
+    else{
+        System.out.println(i+" odd");
+}
+}
+}
+}
