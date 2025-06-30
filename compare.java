d9387c6ccf6d769588471d028334791833a10039
snehal-123-cpu <snehalmkale2005@gmail.com>
2025-06-30 17:33:21 +0530

    Add files via upload

---
+++ compare.java
@@ -0,0 +1,13 @@
+public class compare {
+    public static void main(String[] args) {
+        String str1 = "Java";
+        String str2 = "java";
+
+        if (str1.equals(str2)) {
+            System.out.println(" equal.");
+        } else {
+            System.out.println("not equal.");
+        }
+    }
+}
+
