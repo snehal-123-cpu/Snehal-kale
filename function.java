5612f49c61b4669c2d2d194573a89497bafaa2d6
snehal-123-cpu <snehalmkale2005@gmail.com>
2025-06-30 17:06:33 +0530

    Add files via upload

---
+++ function.java
@@ -0,0 +1,11 @@
+public class function{
+
+    public static void printMyName(String Name){
+System.out.println(Name);
+    }
+        public static void main(String[] args) {
+            printMyName ("Snehal Machindra kale");
+        }
+    }
+    
+
