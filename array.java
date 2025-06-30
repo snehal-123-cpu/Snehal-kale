e007b6234e479094d867e8336661325fd8838d83
snehal-123-cpu <snehalmkale2005@gmail.com>
2025-06-30 17:36:28 +0530

    Add files via upload

---
+++ array.java
@@ -0,0 +1,17 @@
+public class array{
+    public static void main(String[] args) {
+        int[] numbers=new int[5];
+        for( int i=0;i<numbers.length;i++)
+        {
+        numbers [0]=10;
+        numbers [1]=20;
+        numbers [2]=30;
+        numbers [3]=40;
+        numbers [4]=50;
+        
+        {
+    }
+    System.out.println(numbers[i]);
+} 
+}
+}
