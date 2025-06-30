bbb39b5a50ad51b3317533caa5f9d4bb1d0bbfc7
snehal-123-cpu <snehalmkale2005@gmail.com>
2025-06-30 16:45:33 +0530

    Add files via upload

---
+++ oops.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class oops{
+
+    public static void printInfo(){
+    System.out.println("this is print info");
+    }
+        public static void main(String[] args) {
+            Scanner sc=new Scanner (System.in);
+           oops o1=new oops();
+            printInfo ();
+        }
+    }
+    
+
+    
