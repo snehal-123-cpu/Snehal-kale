4f1eb3519d97f68aff4d99a739aa6983d763443f
snehal-123-cpu <snehalmkale2005@gmail.com>
2025-06-30 16:26:28 +0530

    Add files via upload

---
+++ stringbuilder.java
@@ -0,0 +1,11 @@
+ public class stringbuilder {
+    public static void main(String[] args) {
+        StringBuilder Sb = new StringBuilder("Snehal");
+        System.out.println(Sb.charAt(1));
+            Sb.setCharAt('n','e');
+  System.out.println("Sb");
+       
+    }
+}
+
+
