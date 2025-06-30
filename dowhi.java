90a8abdce73681c847b6f1664ad59bad02c588d5
snehal-123-cpu <snehalmkale2005@gmail.com>
2025-06-30 17:14:39 +0530

    Add files via upload

---
+++ dowhi.java
@@ -0,0 +1,10 @@
+public class dowhi {
+    public static void main(String[] args) {
+         System.out.println("\n do-while loop:");
+        int i = 12;
+        do {
+            System.out.println( i);
+            i++;
+        } while (i <= 10);
+    }
+}
