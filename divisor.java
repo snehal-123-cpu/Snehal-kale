da67c58c83868ba47abcd189b91f3d433805d004
snehal-123-cpu <snehalmkale2005@gmail.com>
2025-06-30 17:20:28 +0530

    Add files via upload

---
+++ divisor.java
@@ -0,0 +1,11 @@
+public class divisor {
+    public static void main(String[] args) {
+        int n = 10; 
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
+
