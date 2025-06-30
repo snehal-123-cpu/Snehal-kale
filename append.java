1cc1ec24b39dd53963267c740bd75e997454ea92
snehal-123-cpu <snehalmkale2005@gmail.com>
2025-06-30 17:37:25 +0530

    Add files via upload

---
+++ append.java
@@ -0,0 +1,13 @@
+public class append {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("kale");
+        
+        sb.append('k');
+        sb.append('a');
+        sb.append('l');
+        sb.append('e');
+
+        System.out.println("Result: " + sb.toString());
+    }
+}
+
