8469f82a393fe80d6de23acc87b345e2056cf7a8
snehal-123-cpu <snehalmkale2005@gmail.com>
2025-06-30 16:58:50 +0530

    Add files via upload

---
+++ function4.java
@@ -0,0 +1,14 @@
+   public class function4 {
+
+    public static int printAddition(int a, int b) {
+        int result = a + b;
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int result = printAddition(10, 20);
+        
+        
+        System.out.println("Addition : " + result);
+    }
+}
